64ad63aa7195cc1b84b5b4520a369ba51ae1ec1f
Júlia Botelho <cajujumo@gmail.com>
2022-12-23 14:07:52 -0300

    feat: all routes are functional

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -1,4 +1,12 @@
-export async function retrieveRanks(req, res) {
-    
+import { connectionDB } from "../database/db.js";
 
- }
+export async function retrieveRanks(req, res) {
+
+    try {
+        const rankData = await connectionDB.query('SELECT users.id, users.name, COUNT(urls.userid) AS "linksCount", SUM(urls.visits) AS "visitCount" FROM urls JOIN users ON users.id = urls.userid GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;')
+
+        res.send(rankData.rows).status(200);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,5 @@
+import { connectionDB } from "../database/db.js";
+
 export async function retrieveData(req, res) {
     const { authorization } = req.headers;
 
@@ -6,12 +8,29 @@ export async function retrieveData(req, res) {
         return res.status(401).send("Sua sessão expirou");
     }
 
-    const tokenExists = await connectionDB.query('SELECT token FROM sessions WHERE token = $1',[token]);
-    if (tokenExists.rows.length === 0){
+    const tokenExists = await connectionDB.query('SELECT token FROM sessions WHERE token = $1', [token]);
+    if (tokenExists.rows.length === 0) {
         res.sendStatus(404)
         return
     }
 
-    const userData = await connectionDB.query('SELECT users.id FROM users JOIN sessions ON sessions.userid = users.id WHERE token = $1',[token]);
-    const userId = userData.rows[0];
+    const userData = await connectionDB.query('SELECT users.id FROM users JOIN sessions ON sessions.userid = users.id WHERE token = $1', [token]);
+    const user = userData.rows[0].id;
+
+    try {
+        const userSet = await connectionDB.query('SELECT users.id, users.name, SUM(urls.visits) AS "visitCount" FROM users JOIN urls ON urls.userid = users.id WHERE users.id = $1 GROUP BY users.id;',[user])
+
+        const urlObjects = await connectionDB.query('SELECT urls.id, urls.shorturl AS "shortUrl", urls.url, urls.visits AS "visitCount" FROM urls WHERE urls.userid = $1;',[user])
+
+        const objToGo = {
+            id: userSet.rows[0].id,
+            name: userSet.rows[0].name,
+            visitCount: userSet.rows[0].visitCount,
+            shortenedUrls: urlObjects.rows
+        }
+        res.status(200).send(objToGo)
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
 }
